cc491991fedd66261c141ba6362988669184c11a
randyaf <randyaf29@gmail.com>
2020-06-14 15:35:29 +0700

    fixed the window-title module

--- polybar/scripts/window-title.sh
+++ polybar/scripts/window-title.sh
@@ -2,10 +2,13 @@
 
 windowid="$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')"
 
-if [ $windowid != '0x0' ];
+if [ "$windowid" == '0x0' ];
 then
+    echo 'MANJARO'
+elif [ -z "$windowid" ] 
+then
+    echo 'MANJARO'
+else
     preprint="$(xprop -id $windowid WM_CLASS | awk '{print $4}' | tr -d '"')"
     echo ${preprint^^}
-else
-    echo 'MANJARO'
 fi
